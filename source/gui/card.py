b53101d67186a81df7a70d6b526e5b1d4a70c1d2
Firegregor <gkot35@gmail.com>
2019-11-24 20:02:28 +0100

    vertical lines and moving dates from blank

--- source/gui/card.py
+++ source/gui/card.py
@@ -15,6 +15,7 @@ month = {x+1: y for x, y in enumerate(['styczeń', 'luty', 'marzec', 'kwiecień'
                                        'sierpień', 'wrzesień', 'październik', 'listopad', 'grudzień'])}
 config.params["visible day"] = 5
 config.params["cycle start"] = dt.datetime.now()
+config.params["card"] = {'canvas':(1000, 600)}
 
 
 class Card(ttk.LabelFrame):
@@ -28,7 +29,8 @@ class Card(ttk.LabelFrame):
         self.Log("update started")
         if self.display is not None:
             self.display.pack_forget()
-        self.display = tk.Canvas(self, width=1000, height=600, bg='white')
+        width, heigth = config.params["card"]['canvas']
+        self.display = tk.Canvas(self, width=width, height=heigth, bg='white')
         self.blank()
         self.draw_numbers()
         self.draw_data()
@@ -49,14 +51,31 @@ class Card(ttk.LabelFrame):
         self.display.create_line(off_x, off_y, off_x + 40 * field_width, off_y, width=2)
         self.display.create_text(off_x + 0.5 * field_width, top + 0.3 * field_height, text=month[5], anchor=tk.W) # TODO: Make months from cycle module
         # widoczne dni
-        for day in range(config.params["visible day"], config.params["visible day"] + 40):
-            x= day - config.params["visible day"] + 1
+        for x in range(40):
             if 0:#self.master.menu:
                 if x == self.master.menu.day.get() - 1:
                     self.display.create_rectangle(off_x + x * field_width, bottom + field_height,
                                                   off_x + (x + 1) * field_width, bottom + 2 * field_height, fill='green')
             self.display.create_line(off_x + x * field_width, off_y,
                                      off_x + x * field_width, 580)
+        self.display.create_line(off_x, 20, off_x, 580, width=2)
+        self.display.create_line(off_x + 40 * field_width, top, off_x + 40 * field_width, 580, width=2)
+        # linie temperatury i dni cyklu
+        for level in range(16):
+            self.display.create_line(off_x, off_y + level * field_height, off_x + 40 * field_width,
+                                     off_y + level * field_height, width = (level//14+1))
+        self.Log("Blank done")
+
+    def draw_numbers(self):
+        """
+        Rysowanie zakresu temperatur i dni miesiąca
+        """
+        self.Log("Numbers started")
+        for cycle_day in range(config.params["visible day"], config.params["visible day"] + 40):
+            x= cycle_day - config.params["visible day"] + 1
+            self.display.create_text(off_x + (x-0.5) * field_width,
+                                     bottom + 1.5 * field_height, text=cycle_day)
+            # Dni miesiąca
             day = config.params["cycle start"] + dt.timedelta(days=x - 1)
             if x % 2:
                 self.display.create_text(off_x + (x - 0.5) * field_width,
@@ -64,18 +83,6 @@ class Card(ttk.LabelFrame):
             if 5 <= day.weekday() <= 6:
                 self.display.create_line(off_x + x * field_width, off_y, off_x + x * field_width,
                                          off_y + field_height, width=2)
-        self.display.create_line(off_x, 20, off_x, 580, width=2)
-        self.Log("Blank done")
-
-    def draw_numbers(self):
-        """
-        Rysowanie zakresu temperatur
-        """
-        self.Log("Numbers started")
-        for day in range(config.params["visible day"], config.params["visible day"] + 40):
-            x= day - config.params["visible day"] + 1
-            self.display.create_text(off_x + (x-0.5) * field_width,
-                                     bottom + 1.5 * field_height, text=day)
 
     def draw_data(self):
         """
