57dd3d1fd5f7636e31967f93e13225981c8dc319
Grzegorz Kot <gkot1@sii.pl>
2021-10-21 12:22:31 +0200

    unittest enable

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
+import sys
 import logging
+import unittest
 from src import TkViev, JsonModel ,NRP
+from test import model_tests
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
     app = NRP(JsonModel, TkViev)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 0:
+        unittest.main()
+    else:
+        main()
